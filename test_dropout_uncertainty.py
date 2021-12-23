2fa389ee359489712aa34254a8ab040b9370be35
Anique <aniquetahir@pm.me>
2021-12-22 17:22:36 -0700

    Added a file for trying the dropout uncertainty idea

--- test_dropout_uncertainty.py
+++ test_dropout_uncertainty.py
@@ -72,112 +72,7 @@ def flatten_reshape(x):
     return reshaped_x
 
 
-def train_environment_predictor():
-    # TODO incorporte information about the labels in the environment classifier
-    num_epochs = 6000
 
-    input_shape = training_envs[0][0][0].shape
-    flattened_input_shape = np.prod(list(input_shape))
-    num_hidden_features = 64
-    num_domains = len(training_envs) + len(test_envs)
-
-    one_hot_domains = torch.nn.functional.one_hot(torch.arange(num_domains), num_domains).to(device)
-
-    # Define the sequence classification model
-    environment_detector = torch.nn.GRU(input_size = flattened_input_shape,
-                                        hidden_size = num_hidden_features,
-                                        num_layers = 1,
-                                        batch_first = True,
-                                        bidirectional = True).to(device)
-
-    environment_predictor = torch.nn.Sequential(
-        torch.nn.Linear(2 * num_hidden_features + num_domains, 512),
-        torch.nn.Linear(512, 2)
-    ).to(device)
-
-    # if os.path.exists('environment_predictor.pth') and os.path.exists('environment_detector.pth'):
-    #     environment_predictor = torch.load('environment_predictor.pth')
-    #     environment_detector = torch.load('environment_detector.pth')
-    #     # Since model is already trained, we ensure it is doing the right thing
-    #     num_epochs = 100
-
-    all_params = list(environment_detector.parameters()) + list(environment_predictor.parameters())
-
-    optimizer = torch.optim.Adam(
-        all_params,
-        lr=0.003
-    )
-    train_generators = [data_generator(x) for x in training_envs]
-    test_generators = [data_generator(x) for x in test_envs]
-    all_generators = train_generators + test_generators
-    all_envs = training_envs + test_envs
-
-    for j in range(num_epochs):
-        # For each training environment
-        mix_ratio = torch.tensor(np.random.uniform(size=num_domains)).to(device)
-        mix_ratio = mix_ratio/torch.sum(mix_ratio)
-
-        pred_envs = []
-        gt_envs = []
-        x_envs = []
-
-        for i, env in enumerate(all_envs):
-            # Get the sequence of the samples
-            env_batch_x, _, _, _ = next(all_generators[i])
-            batch2_x, _, _, _ = next(all_generators[i])
-
-            num_env_samples = len(env_batch_x)
-            num_filtered_samples = int(num_env_samples * mix_ratio[i])
-
-
-            filtered_samples_indices = np.random.choice(np.arange(num_env_samples), size=num_filtered_samples, replace=False)
-            filtered_batch = env_batch_x[filtered_samples_indices]
-            x_envs.append(filtered_batch)
-            gt = torch.ones(num_filtered_samples).to(device) * i
-            #reshaped_x = flatten_reshape(env_batch_x)
-            # flat_x = torch.flatten(env_batch_x, start_dim=1)
-            # reshaped_x = flat_x.reshape(torch.cat([torch.tensor([1]), torch.tensor(flat_x.shape)]).tolist())
-            #m_env_feats = environment_detector(reshaped_x)[0][0]
-            #preds = environment_predictor(m_env_feats)
-            #pred_envs.append(preds)
-            gt_envs.append(gt)
-
-        x_envs = torch.vstack(x_envs)
-        gt_envs = torch.cat(gt_envs)
-
-        batch_indices = np.arange(len(gt_envs))
-        # change the number of classification samples to make sure that the batch classification works on
-        # different lengths of the sequences
-        np.random.shuffle(batch_indices)
-        num_selections = np.random.randint(len(batch_indices)) + 1
-        batch_indices = batch_indices[:num_selections]
-        x_envs = x_envs[batch_indices]
-        gt_envs = gt_envs[batch_indices]
-
-        reshaped_x = flatten_reshape(x_envs)
-        m_feats = environment_detector(reshaped_x)[0][0]
-
-        classification_domain = np.random.choice(np.arange(num_domains))
-        gt_prediction = (gt_envs == classification_domain).long()
-        m_feats_input = torch.ones(len(m_feats), 1).to(device) * one_hot_domains[classification_domain]
-        m_feats_input = torch.hstack([m_feats_input, m_feats])
-
-        preds = environment_predictor(m_feats_input)
-        loss_env_prediction = F.cross_entropy(preds, gt_prediction)
-
-        # Get the total loss across the environments
-        optimizer.zero_grad()
-        loss_env_prediction.backward()
-        optimizer.step()
-        # optimize
-        if j % 100 == 0:
-            print(f'Epoch: {j}, Loss: {loss_env_prediction.item()}')
-            print(f'Domain: {classification_domain}, Accuracy: {mean_accuracy(preds, gt_prediction)}')
-
-    torch.save(environment_detector, 'environment_detector.pth')
-    torch.save(environment_predictor, 'environment_predictor.pth')
-    print(loss_env_prediction.item())
-    return environment_detector, environment_predictor
 
 
 def get_color_label_correlation(labels, colors):
@@ -204,21 +99,36 @@ def train_erm(env_detector, env_predictor):
      'lr': 0.005,
      'weight_decay': 0.0,
      'batch_size': 512}
+
+
     featurizer = networks.Featurizer(input_shape, hparams)
     classifier = networks.Classifier(
         featurizer.n_outputs,
         num_classes,
         hparams['nonlinear_classifier']
     )
-    network = nn.Sequential(featurizer, classifier).to(device)
-    optimizer = torch.optim.Adam(
-        network.parameters(),
-        lr = hparams['lr']
-    )
+
+
+    # featurizer = networks.Featurizer(input_shape, hparams)
+    # classifier = networks.Classifier(
+    #     featurizer.n_outputs,
+    #     num_classes,
+    #     hparams['nonlinear_classifier']
+    # )
+    # network = nn.Sequential(featurizer, classifier).to(device)
+    # optimizer = torch.optim.Adam(
+    #     network.parameters(),
+    #     lr = hparams['lr']
+    # )
     train_generators = [data_generator(x, batch_size=batch_size) for x in training_envs]
     test_generators = [data_generator(x) for x in test_envs]
     num_train_environments = len(train_generators)
     num_all_envs = num_train_environments + len(test_generators)
+
+    environment_networks = []
+
+    for i in range(num_train_environments):
+        pass
 
     one_hot_envs = torch.nn.functional.one_hot(torch.arange(num_all_envs), num_all_envs).to(device)
 
@@ -297,12 +207,6 @@ def train_erm(env_detector, env_predictor):
             print(f'Test Accuracy: {test_acc}, True: {test_true_acc}')
             print(f'E1 Accuracy: {e1_acc}, True: {e1_true_acc}')
             print(f'E2 Accuracy: {e2_acc}, True: {e2_true_acc}')
-    pass
-
-
-def environment_generator(env_detector, env_predictor):
-    # Get data from the training and test environments following a random distribution
-    # We will learn the same thing in the generator that we learned in the predictor
     pass
 
 if __name__ == "__main__":
